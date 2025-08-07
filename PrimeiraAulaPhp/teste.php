bb87d0bf33e346d58050c835db98e7a1c2c67256
Besu <bbesu1606@gmail.com>
2025-08-07 20:22:36 -0300

    Add files via upload

---
+++ PrimeiraAulaPhp/teste.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="Besu" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Aula Php</h1>
+    <?php
+    echo "Ola mundo!";
+
+    //comentario barra barra
+    #comentario hashtag
+    /*comentario 
+        bloco*/  ?>
+        <br>
+             <br>
+                  <br>
+
+
+<?php
+$base = 10;
+$altura = 8;
+$area = $base * $altura;
+echo
+"A área do retângulo é: " . $area;
+    ?>
+<br>
+<br>
+<?php
+
+$x = 10;
+echo ($x + 5);
+echo "<br/>";
+
+echo ($x - 5);
+echo "<br/>";
+
+echo ($x * 5);
+echo "<br/>";
+
+echo ($x / 5);
+echo "<br/>";
+
+echo ($x % 8);
+echo "<br/>";
+?>
+
+</body>
+</html>
